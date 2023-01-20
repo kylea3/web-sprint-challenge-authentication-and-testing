1d7755a42da014b938f3bec1e99d220d83c7568c
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-19 17:14:14 -0700

    add endpoint tests for post register, post login, and get jokes

--- api/server.test.js
+++ api/server.test.js
@@ -1,6 +1,56 @@
-// Write your tests here
+const db = require('../data/dbConfig')
+const request = require('supertest')
+const server = require('./server')
+
+const user = { username: 'Thor', password:'foobar' } 
 test('sanity', () => {
   expect(true).toBe(true)
 })
 
-beforeAll()
+beforeAll(async () => {
+    await db.migrate.rollback()
+    await db.migrate.latest()
+})
+
+describe('[POST] /api/auth/register', () => {
+    test('responds with 201', async () => {
+        const res = await request(server).post('/api/auth/register').send(user)
+        expect(res.status).toBe(201)
+    })
+    test('responds with correct error code on same username', async () => {
+        await request(server).post('/api/auth/register').send(user)
+        const res = await request(server).post('/api/auth/register').send(user)
+        expect(res.status).toBe(404)
+    })
+})
+
+describe('[POST] /api/auth/login', () => {
+    test('responds with 200', async () => {
+      await request(server).post('/api/auth/register')
+        const res = await request(server).post('/api/auth/login').send(user)
+        expect(res.status).toBe(200)
+    })
+    test('responds with 404 if username or password missing', async () => {
+      await request(server).post('/api/auth/register')
+        let res = await request(server).post('/api/auth/login').send({ username: 'Thor'})
+        expect(res.status).toBe(404)
+        res = await request(server).post('/api/auth/login').send({ password: 'foobar'})
+        expect(res.status).toBe(404)
+    })
+})
+
+describe('[GET] /api/jokes', () => {
+  test('token required code on no token', async () => {
+    await request(server).post('/api/auth/register')
+    await request(server).post('/api/auth/login').send(user)
+    const res = await request(server).get('/api/jokes')
+    expect(res.status).toBe(401)  
+  })
+  test('responds with jokes if token valid', async () => {
+    await request(server).post('/api/auth/register')
+    let res = await request(server).post('/api/auth/login').send(user)
+    const token = res.body.token
+    res = await request(server).get('/api/jokes').set('Authorization', token)
+    expect(res.status).toBe(200) 
+  })
+})
